e8338b84c8bf3ee47d23a942e7364073ee52b12b
VUNz <92833169+phivumop@users.noreply.github.com>
2022-12-24 21:10:30 +0700

    Add files via upload

---
+++ on tap javascript.js
@@ -0,0 +1,24 @@
+/* cau 1:
+const number = [4, 5, 6, 7, 8];
+function findEven(number) {
+    if ( number % 2 == 0) {
+        alert('Even Number');
+}
+} */
+
+/* cau 2:
+document.getElementById("touch").addEventListener("mouseenter", mouseEnter);
+document.getElementById("touch").addEventListener("mouseleave", mouseLeave);
+
+function mouseEnter() {
+  document.getElementById("touch").style.color = "red";
+}
+
+function mouseLeave() {
+  document.getElementById("touch").style.color = "blue";
+}
+*/
+
+/* cau 3:
+    
+*/
